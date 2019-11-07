11d45260bf32d20c385a1ccd5089d6d39f418dd8
lw <642613518@qq.com>
2019-11-07 17:54:32 +0800

    调接口

--- src/app/demand/demand.component.ts
+++ src/app/demand/demand.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, ElementRef, OnInit } from "@angular/core";
+import {ApiServer} from "../apis/api.server";
+import { ElMessageService } from "element-angular";
 
 
 @Component({
@@ -8,10 +10,35 @@ import {Component, OnInit} from "@angular/core";
 })
 
 export class DemandComponent implements OnInit{
-    constructor(){
+    public nick_name:string = "";
+    public contact:string = "";
+    public remarks:string = "";
+    public loading:boolean = false;
+    constructor(private api: ApiServer, private message: ElMessageService, private el: ElementRef){
 
     }
     ngOnInit(){
 
+    }
+    submit(){
+        this.loading = true;
+        let data = {
+            nick_name : this.nick_name ,
+            contact : this.contact ,
+            remarks : this.remarks
+        };
+        this.api.subscribe( data ).then(res => {
+            this.loading = false;
+            switch ( res.code ) {
+                case 200 :
+                    this.message.success("提交成功");
+                    break;
+                default :
+                    this.message.error( res.msg );
+                    this.el.nativeElement.querySelector("#contact").focus();
+            }
+        }, err => {
+            this.loading = false;
+        })
     }
 }
